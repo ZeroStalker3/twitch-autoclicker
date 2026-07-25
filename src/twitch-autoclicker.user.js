d47521c0ac62288474385c503d3f83fa322cb930
ZeroYz <81680666+ZeroStalker3@users.noreply.github.com>
2026-07-26 02:56:32 +0900

    Update version number to 2.5.6

--- src/twitch-autoclicker.user.js
+++ src/twitch-autoclicker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitch Auto Bonus Clicker
 // @namespace    https://github.com/ZeroStalker3/twitch-autoclicker
-// @version      0.0.0
+// @version      2.5.6
 // @description  Автоматический сбор бонусов на Twitch с GUI, логированием и имитацией поведения
 // @author       ZeroYz
 // @match        *://*.twitch.tv/*
