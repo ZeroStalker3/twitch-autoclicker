9653a0f993d668a6558610683fa4fe59d11bb959
ZeroYz <stalker_mr@list.ru>
2026-08-12 22:51:37 +0900

    chore: bump to v2.6.1

--- src/twitch-autoclicker.user.js
+++ src/twitch-autoclicker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitch Auto Bonus Clicker
 // @namespace    https://github.com/ZeroStalker3/twitch-autoclicker
-// @version      2.6.0
+// @version      2.6.1
 // @description  Автоматический сбор бонусов на Twitch с GUI, логированием и имитацией поведения
 // @author       ZeroYz
 // @match        *://*.twitch.tv/*
